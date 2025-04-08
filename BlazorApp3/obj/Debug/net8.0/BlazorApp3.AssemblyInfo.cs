82eaf75090aac87c781d052b76be00815d41dcf8
Anastasiya Burenok <nastya.burenok12@yandex.ru>
2025-04-08 19:11:41 +0300

    Теперь с нормальным временем

--- BlazorApp3/obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
+++ BlazorApp3/obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e93588dea87baa97de11c5a7c73cb9ce30400cf8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd7a6c083d3da8ecb825434159d7213bb318b48")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
