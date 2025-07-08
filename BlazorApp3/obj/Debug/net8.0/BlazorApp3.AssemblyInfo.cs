ddc4ef9f6b286d38ce852c11c4b5eab9ab85db5a
Anastasiya Burenok <nastya.burenok12@yandex.ru>
2025-07-08 22:10:41 +0300

    еще разок

--- BlazorApp3/obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
+++ BlazorApp3/obj/Debug/net8.0/BlazorApp3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bff5558737acc061161576c78971c59c5ba7b2c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86c11d2b5ee3e7f29f9d26274997f254e5dad81e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
